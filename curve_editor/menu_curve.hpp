18c6b47efc2ddba7e6316351b589c10bd90d1b44
mimaraka <nj.ot.niconico@gmail.com>
2024-04-15 01:29:39 +0900

    変数名を変更

--- curve_editor/menu_curve.hpp
+++ curve_editor/menu_curve.hpp
@@ -7,15 +7,15 @@
 namespace cved {
 	class CurveMenu : public Menu {
 		HMENU submenu_segment_type_;
-		void update_state(size_t index) noexcept;
-		bool callback(size_t index, uint16_t id) noexcept;
+		void update_state(size_t idx) noexcept;
+		bool callback(size_t idx, uint16_t id) noexcept;
 
 	public:
 		CurveMenu(HINSTANCE hinst);
 
-		HMENU get_handle(size_t index) noexcept;
+		HMENU get_handle(size_t idx) noexcept;
 		int show(
-			size_t index,
+			size_t idx,
 			HWND hwnd,
 			UINT flags = NULL,
 			const mkaul::Point<LONG>* p_custom_point_screen = nullptr
